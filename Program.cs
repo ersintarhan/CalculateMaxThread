4a85ee4404185a0f27ea37aaf5250db05262fdb0
Ersin Tarhan <ersintarhan@gmail.com>
2020-10-08 19:41:32 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace CalculateMaxThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int threadCount = 0;
+            try
+            {
+                for (int i = 0; i < int.MaxValue; i ++)
+                {
+                    new Thread(() => Thread.Sleep(Timeout.Infinite)).Start();
+                    threadCount ++;
+                }
+            }
+            catch
+            {
+                Console.WriteLine(threadCount);
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
